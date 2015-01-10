84303162437b57ba078aaf8aa417c7d4056a0a5e
Robert Sumner <rcsumner@ucsc.edu>
2015-01-10 09:41:15 -0800

    Testing Android Studio's built-in Git functions.

--- MyApplication/app/src/main/java/com/spacehackteam/wifitests/MainActivity.java
+++ MyApplication/app/src/main/java/com/spacehackteam/wifitests/MainActivity.java
@@ -1,5 +1,10 @@
 package com.spacehackteam.wifitests;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -8,10 +13,33 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private final IntentFilter intentFilter = new IntentFilter();
+    WifiP2pManager.Channel mChannel;
+    WifiP2pManager mManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // - - - Set up intent filter so system alerts about wifi get parsed by app - - -
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+
+        // Get the wifi manager and a channel object from its initialization
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this,getMainLooper(),null);
+
     }
 
 
@@ -36,4 +64,13 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private class WifiBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent){
+
+        }
+    }
+
 }
