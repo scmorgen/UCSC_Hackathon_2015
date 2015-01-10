def24cbb47f776030c6b8f3998a221f7220f4552
scmorgen <scmorgen@users.noreply.github.com>
2015-01-10 10:15:27 -0800

    Can take in messages (f:Phone to:Pebble)

---
+++ src/BackgroundFcns.c
@@ -0,0 +1,3 @@
+#include <pebble_worker.h>
+#include "BackgroundFcns.h"
+
--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,24 +1,44 @@
+
+
 // Function to send a message to the Pebble using AppMessage API
-function sendMessage() {
-	Pebble.sendAppMessage({"status": 0});
-	
-	// PRO TIP: If you are sending more than one message, or a complex set of messages, 
-	// it is important that you setup an ackHandler and a nackHandler and call 
-	// Pebble.sendAppMessage({ /* Message here */ }, ackHandler, nackHandler), which 
-	// will designate the ackHandler and nackHandler that will be called upon the Pebble 
-	// ack-ing or nack-ing the message you just sent. The specified nackHandler will 
-	// also be called if your message send attempt times out.
+
+function HTTPGET(url) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send(null);
+    return req.responseText;
 }
 
+var getWeather = function() {
+    //Get weather info
+    var response = HTTPGET("http://api.openweathermap.org/data/2.5/weather?q=London,uk");
+ 
+    //Convert to JSON
+    var json = JSON.parse(response);
+ 
+    //Extract the data
+    var temperature = Math.round(json.main.temp - 273.15);
+    var location = json.name;
+ 
+    //Console output to check all is working.
+    console.log("It is " + temperature + " degrees in " + location + " today!");
+ 
+    //Construct a key-value dictionary
+    var dict = { "KEY_TEMPERATURE": temperature, "KEY_LOCATION" : location};
+ 
+    //Send data to watch for display
+    Pebble.sendAppMessage(dict);
+};
 
 // Called when JS is ready
 Pebble.addEventListener("ready",
 							function(e) {
+                getWeather();
 							});
 												
 // Called when incoming message from the Pebble is received
-Pebble.addEventListener("appmessage",
-							function(e) {
-								console.log("Received Status: " + e.payload.status);
-								sendMessage();
-							});
+//Pebble.addEventListener("appmessage",
+							//function(e) {
+							//	console.log("Received Status: " + e.payload.status);
+							//	sendMessage();
+							//});
